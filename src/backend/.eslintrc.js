7c636802563e517379e958627568dacb76281f12
benzzy <92602899+benzdriver@users.noreply.github.com>
2025-04-12 12:50:04 -0700

    Updated lint info

---
+++ src/backend/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      tsconfigRootDir: __dirname,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    root: true,
+    env: {
+      node: true,
+      jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js', 'node_modules/', 'dist/'],
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { 
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'caughtErrorsIgnorePattern': '^_'
+      }],
+      'prettier/prettier': ['error', {
+        'endOfLine': 'auto',
+        'singleQuote': true,
+        'trailingComma': 'all',
+        'printWidth': 100,
+        'tabWidth': 2
+      }],
+      'no-console': ['warn', { 'allow': ['warn', 'error'] }],
+      'prefer-const': 'warn',
+      'no-var': 'error',
+      'eqeqeq': ['error', 'always', {'null': 'ignore'}],
+    },
+  };
+  
---
+++ src/frontend/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      tsconfigRootDir: __dirname,
+      sourceType: 'module',
+      ecmaVersion: 2020,
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'react', 'react-hooks'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+    ],
+    root: true,
+    env: {
+      node: true,
+      browser: true,
+      es6: true,
+    },
+    ignorePatterns: ['.eslintrc.js', 'node_modules/', 'dist/', 'build/'],
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { 
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'caughtErrorsIgnorePattern': '^_'
+      }],
+      'no-console': 'warn',
+      'prefer-const': 'warn',
+      'no-var': 'error',
+      'eqeqeq': ['error', 'always', {'null': 'ignore'}],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
+  
